6ab3bd890e448b9daa0dd48873991e550b4df2f3
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-05-01 17:53:07 -0700

    logic for seasons
    utilizing user geolocation latitude and Date().getMonth to determine season based on hemisphere.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,20 @@
-import react from "react";
+import React from "react";
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  console.log(season);
   return (
     <div>
-        <h2></h2>     
+        <p></p>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    //can be a good place to initialize state
-    super(props);
-    this.state = { lat: null };
+  state = { lat: null, errorMessage: '' };
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-          this.setState({ lat: position.coords.latitude });
-      },
-      (err) => console.log(err)
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
+
   //React says we have to define a render method
   render() {
-    return <div>Latitude: {this.state.lat} </div>;
+    //if error (user denies geolocation)
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    //if lat returns
+    if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <SeasonDisplay lat={this.state.lat} />
+      );
+    }
+    //when loading
+    return <div>Loading...</div>;
   }
 }
 
